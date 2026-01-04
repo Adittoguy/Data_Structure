2d923ff4cff4c72f4db7d6c4b0a662e4db830709
Adittoguy <adityasanap2001@gmail.com>
2026-01-04 23:46:36 +0530

    Generic LL

--- LinkedList/DoublyCL.cpp
+++ LinkedList/DoublyCL.cpp
@@ -30,25 +30,192 @@ class DoublyCL
         }
 
         void InsertFirst(int no)
-        {}
+        {
+            PNODE newn = NULL;
+
+            newn = new NODE;
+
+            newn -> data = no;
+            newn -> next = NULL;
+            newn -> prev = NULL;
+
+            if((first == NULL) && (last == NULL))
+            {
+                first = newn;
+                last = newn;
+            }
+            else
+            {
+                newn -> next = first;
+                first -> prev = newn;
+                first = newn;
+            }
+            iCount++;
+            last -> next = first;
+            first -> prev = last;
+        }
         
         void InsertLast(int no)
-        {}
+        {
+            PNODE newn = NULL;
+
+            newn = new NODE;
+
+            newn -> data = no;
+            newn -> next = NULL;
+            newn -> prev = NULL;
+
+            if((first == NULL) && (last == NULL))
+            {
+                first = newn;
+                last = newn;
+            }
+            else
+            {
+                last -> next = newn;
+                newn -> prev = last;
+                last = newn;
+            }
+            iCount++;
+            last -> next = first;
+            first -> prev = last;
+        }
 
         void DeleteFirst()
-        {}
+        {
+            if(first == NULL && last == NULL)
+            {
+                return;
+            }
+            else if(first == last)
+            {
+                free(first);
+                first = NULL;
+                last = NULL;
+            }
+            else
+            {
+                first = first -> next;
+                free(first -> prev);
+
+                last -> next = first;
+                first -> prev = last;
+            }
+            iCount--;
+        }
 
         void DeleteLast()
-        {}
+        {
+            if(first == NULL && last == NULL)
+            {
+                return;
+            }
+            else if(first == last)
+            {
+                free(first);
+                first = NULL;
+                last = NULL;
+            }
+            else
+            {
+                last = last -> prev;
+                free(last -> next);
+
+                last -> next = first;
+                first -> prev = last;
+            }
+            iCount--;
+        }
 
         void InsertAtPos(int no, int pos)
-        {}
+        {
+            int iCnt = 0;
+            PNODE newn = NULL;
+            PNODE temp = NULL;
+
+            if(pos == 1)
+            {
+                InsertFirst(no);
+            }
+            else if(pos == iCount + 1)
+            {
+                InsertLast(no);
+            }
+            else
+            {
+                temp = first;
+                newn = new NODE;
+                newn -> data = no;
+                newn -> next = NULL;
+                newn -> prev = NULL;
+
+                for(iCnt = 1; iCnt < pos - 1; iCnt++)
+                {
+                    temp = temp -> next;
+                }
+                newn -> next = temp -> next;
+                newn -> next -> prev = newn;
+                temp -> next = newn;
+                newn -> prev = temp;
+
+                last -> next = first;
+                first -> prev = last;
+
+                iCount++;
+            }
+        }
 
         void DeleteAtPos(int pos)
-        {}
+        {
+            int iCnt = 0;
+            PNODE temp = NULL;
+
+            if((pos < 1) || (pos >iCount))
+            {
+                cout<<"Invalid Position";
+                return;
+            }
+
+            if(pos == 1)
+            {
+                DeleteFirst();
+            }
+            else if(pos == iCount)
+            {
+                DeleteLast();
+            }
+            else
+            {
+                temp = first;
+
+                for(iCnt = 1; iCnt < pos - 1; iCnt++)
+                {
+                    temp = temp -> next;
+                }
+                temp -> next = temp -> next -> next;
+                free(temp -> next -> prev);
+                temp -> next -> prev = temp;
+
+                iCount--;
+            }
+        }
 
         void Display()
-        {}
+        {
+            if(first == NULL && last == NULL)
+            {
+                cout<<"List is Empty\n";
+                return;
+            }
+
+            cout<<"<==>";
+            do
+            {
+                cout<<"| "<<first->data<<" |<==>";
+                first = first -> next;
+            }while(first != last -> next);
+            cout<<"\n";
+        }
 
         int Count()
         {
